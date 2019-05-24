ed0ead2d40be147f9c04435bfd56ce13fc5e825e
Steven Bassett <steven.j.bassett@gmail.com>
2019-05-24 10:01:28 -0700

    style(prettier): config and reformat code style

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  jsxBracketSameLine: false,
+  bracketSpacing: true,
+  trailingComma: true,
+  jsxSingleQuote: false,
+  quoteProps: 'as-needed',
+};
--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,14 +1,12 @@
-import { Component, h } from "preact";
-import * as style from "./style.css";
+import { h } from 'preact';
+import { home } from './style.css';
 
 interface Props {}
-export default class Home extends Component<Props> {
-    public render() {
-        return (
-            <div class={style.home}>
-                <h1>Home</h1>
-                <p>This is the Home component.</p>
-            </div>
-        );
-    }
+export default function Home() {
+  return (
+    <div className={home}>
+      <h2 className="">Home</h2>
+      <p>This is the Home component.</p>
+    </div>
+  );
 }
